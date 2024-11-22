7d8904e11297ab0877fe20a9bed170bf027910ce
julu <juanluismatalop@gmail.com>
2024-11-22 13:13:33 +0100

    commit 22/11

---
+++ tema2/VolumenI/Practica/ejercicio1.js
@@ -0,0 +1,30 @@
+// a. Crea una cadena multilínea usando comillas dobles.
+let cadena = ("Hola mundo" + " soy Juan Luis");
+console.log(cadena);
+// b. Agrega a la cadena un retorno de carro y tabuladores utilizando los símbolos de escape corres‑
+// pondientes.
+cadena = ("Hola mundo" + '\n' + " soy Juan Luis");
+console.log(cadena);
+// c. Incluye en la cadena el carácter \.
+// d. Concatena otra cadena utilizando el operador +.
+// e. Concatena cadenas usando un template string. Muestra el valor de varias variables en el tem‑
+// plate string.
+// f. Separa un texto que contenga varias frases en un array, donde cada elemento del array sea una
+// frase.
+// g. Convierte un texto dado a minúsculas.
+// h. Convierte un texto dado a mayúsculas.
+// i. Recorre el texto carácter por carácter usando un bucle e imprímelo.
+// j. Busca una subcadena dentro de un texto.
+// k. Extrae una subcadena desde la posición 3 hasta el final del texto y guárdala en una variable.
+// l. Extrae una subcadena desde la posición 3 hasta la primera ocurrencia de una palabra en el texto
+// y guárdala en una variable.
+// m. Reemplaza todos los espacios en el texto por un guion ‑.
+// n. Elimina los espacios en blanco antes y después del texto.
+// o. Crea una cadena que no contenga ningún espacio partiendo de otra cadena dada.
+// p. Crea una función que invierta una cadena de texto.
+// q. Usa una expresión regular para comprobar si la cadena contiene números.
+// r. Usa una expresión regular para comprobar si la cadena termina en un punto.
+// s. Usa una expresión regular para comprobar si la cadena comienza con una mayúscula.
+// t. Usa una expresión regular para comprobar si la cadena contiene un número de teléfono con
+// código internacional.
+// u. Reemplaza cualquier ocurrencia de un + seguido de números por la cadena “SECRETO”.
